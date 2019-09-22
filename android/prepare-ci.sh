963003e420a1ec14d5dfd596b01a7afc9577ccd4
Hans Hoffman <hans@fourbytes.co>
2019-09-22 19:21:09 -0400

    fixed dirs

--- android/prepare-ci.sh
+++ android/prepare-ci.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 
 printf "[+] Setting up env vars...\n"
+cd ~
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
-env | grep ANDROID
-echo $PATH
 
 printf "[+] Installing Android SDK...\n"
-cd ~
-wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-unzip sdk-tools-linux-4333796.zip -d android_sdk
-
-printf "[+] Accepting Android SDK licenses...\n"
+wget -q https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+unzip -q sdk-tools-linux-4333796.zip -d android_sdk
 yes | sdkmanager --licenses && yes | sdkmanager --update || exit 1
 
 printf "[+] Installing Fastlane...\n"
-cd android
+cd ~/react-native/android
 gem install bundler
 bundle update && bundle install

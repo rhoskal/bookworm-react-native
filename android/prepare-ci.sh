45ff6112f5fa1932cb4d24c3ca406051ac130047
Hans Hoffman <hans@fourbytes.co>
2019-09-22 19:11:54 -0400

    moved to instlling sdk from zip

--- android/prepare-ci.sh
+++ android/prepare-ci.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-printf "[+] Installing Android SDK...\n"
-sudo apt install -y android-sdk
 
 printf "[+] Setting up env vars...\n"
-export ANDROID_HOME=/usr/lib/android-sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+env | grep ANDROID
+echo $PATH
+
+printf "[+] Installing Android SDK...\n"
+cd /home
+wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+unzip sdk-tools-linux-4333796.zip -d android_sdk
 
 printf "[+] Accepting Android SDK licenses...\n"
-tail -f /dev/null
 yes | sdkmanager --licenses && yes | sdkmanager --update || exit 1
 
 printf "[+] Installing Fastlane...\n"

b0b2b6a28cecbaa5aea37b18daa7501b219a8613
Hans Hoffman <hans@fourbytes.co>
2019-09-22 17:56:37 -0400

    why can't I find sdkmanager?

--- android/prepare-ci.sh
+++ android/prepare-ci.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+printf "[+] Installing Android SDK...\n"
+sudo apt install -y android-sdk
+
 printf "[+] Setting up env vars...\n"
 export ANDROID_HOME=/usr/lib/android-sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
@@ -6,8 +9,8 @@ export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-printf "[+] Installing Android SDK...\n"
-sudo apt install -y android-sdk
+printf "[+] Accepting Android SDK licenses...\n"
+tail -f /dev/null
 yes | sdkmanager --licenses && yes | sdkmanager --update || exit 1
 
 printf "[+] Installing Fastlane...\n"

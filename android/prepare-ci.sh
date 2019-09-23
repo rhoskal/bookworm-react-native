d2ec708b3c77f9783d475eacd88f1f48feb9e531
Hans Hoffman <hans@fourbytes.co>
2019-09-22 20:31:16 -0400

    uggggghhhh3

--- android/prepare-ci.sh
+++ android/prepare-ci.sh
@@ -4,6 +4,7 @@ wget -q https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 unzip -q sdk-tools-linux-4333796.zip -d android-sdk
 yes | android-sdk/tools/bin/sdkmanager --licenses
 yes | android-sdk/tools/bin/sdkmanager --update
+export ANDROID_HOME=$HOME/android-sdk
 
 printf "[+] Installing Fastlane...\n"
 cd ~/$SEMAPHORE_GIT_DIR/android

78e633bb9b9469ae1e4e321d2cfa036bd155e51a
Hans Hoffman <hans@fourbytes.co>
2019-09-22 20:24:34 -0400

    uggggghhhh

--- android/prepare-ci.sh
+++ android/prepare-ci.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 printf "[+] Installing Android SDK...\n"
 wget -q https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
-unzip -q sdk-tools-linux-4333796.zip -d android_sdk
-yes | sdkmanager --licenses && yes | sdkmanager --update || exit 1
+unzip -q sdk-tools-linux-4333796.zip -d android-sdk
+yes | android-sdk/tools/bin/sdkmanager --licenses
+yes | android-sdk/tools/bin/sdkmanager --update
 
 printf "[+] Installing Fastlane...\n"
-pwd && cd ~/bookworm-react-native/android
+cd ~/$SEMAPHORE_GIT_DIR/android
 gem install bundler
 bundle update && bundle install
